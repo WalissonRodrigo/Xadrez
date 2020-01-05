d637e306e1303960090385bd0b433a9188cf3a0b
Walisson Rodrigo <walissonrodrigo@outlook.com>
2020-01-05 13:27:42 -0300

    teste básico adicionando peças no tabuleiro

--- Xadrex/Program.cs
+++ Xadrex/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Xadrex.board;
+using Xadrex.chess;
 
 namespace Xadrex
 {
@@ -7,7 +8,11 @@ namespace Xadrex
     {
         static void Main()
         {
+            Console.Title = "Xadrex";
             Board board = new Board(8, 8);
+            board.AddPiece(new Tower(board, Color.Black), new Position(0, 0));
+            board.AddPiece(new Tower(board, Color.Black), new Position(1, 3));
+            board.AddPiece(new King(board, Color.White), new Position(2, 4));
             Screen.PrintBoard(board);
             Console.ReadLine();
         }
