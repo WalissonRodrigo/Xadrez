c455dce2c9f1d44f8a961670086e235bc051c1ca
Walisson Rodrigo <walissonrodrigo@outlook.com>
2020-01-05 12:39:21 -0300

    inclusão da classe Posição do Tabuleiro

---
+++ Xadrex/Board/Position.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xadrex.Board
+{
+    class Position
+    {
+        public int line { get; set; }
+        public int column { get; set; }
+
+        public Position()
+        {
+        }
+
+        public Position(int line, int column)
+        {
+            this.line = line;
+            this.column = column;
+        }
+
+        public override string ToString()
+        {
+            return $"{line}, {column}";
+        }
+    }
+}
