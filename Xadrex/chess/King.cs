923cb5cd2f7deb634ca0dcfbf87cde2b0e7dc8ab
Walisson Rodrigo <walissonrodrigo@outlook.com>
2020-01-05 13:26:16 -0300

    incluso novas peças herdando do tipo Piece generico

---
+++ Xadrex/chess/King.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xadrex.board;
+
+namespace Xadrex.chess
+{
+    public class King : Piece
+    {
+        public King(Board board, Color color) : base(color, board)
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return "R";
+        }
+    }
+}
